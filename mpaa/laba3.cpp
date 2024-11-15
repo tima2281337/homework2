6d536dfea987f25ec6a68ecbaaf5358e31d785e1
Tima <alievtamerlan47@gmail.com>
2024-11-16 01:01:18 +0700

    отчет 3

--- mpaa/laba3.cpp
+++ mpaa/laba3.cpp
@@ -96,14 +96,29 @@ string generateRandomUpperCaseString(int length) {
 
 
 int main() {
-    int desiredLength = 10000; // Задаем желаемую длину строки
+    int desiredLength = 100000; // Задаем желаемую длину строки
     string X = generateRandomUpperCaseString(desiredLength);
     string Y = generateRandomUpperCaseString(desiredLength);
-    
-    
+    auto t1 = steady_clock::now();
 
-    cout << "НОП (" << "X" << ", " << "Y" << ") методом полного перебора: " << lcs_brute_force(X, Y) << endl;
-    cout << "НОП (" << "X" << ", " << "Y" << ") методом динамического программирования: " << lcs_dynamic_programming(X, Y) << endl;
+    string b = lcs_brute_force(X, Y);
+
+    auto t2 = steady_clock::now();
+
+    auto time = duration<double>(t2 - t1).count();
+
+    auto t3 = steady_clock::now();
+
+    string h = lcs_dynamic_programming(X, Y);
+
+  auto t4 = steady_clock::now();
+
+    auto time2 = duration<double>(t4 - t3).count();
+
+
+
+    cout << "НОП (" << "X" << ", " << "Y" << ") методом полного перебора: " << time  << endl;
+    cout << "НОП (" << "X" << ", " << "Y" << ") методом динамического программирования: " << time2 << endl;
 
     return 0;
 }
